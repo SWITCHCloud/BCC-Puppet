d979c97391ebb23f1e326f1447427a89d0e04bb6
Joël Casutt <joel@casuttco.ch>
2012-12-14 14:08:30 +0100

    add a module to manage the users

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,5 @@
 node default {
     include first_file
     include sshd
+    include user
 }
---
+++ modules/user/manifests/sysadmins.pp
@@ -0,0 +1,7 @@
+class user::sysadmins
+{
+    search User::Virtual 
+
+    realize( Ssh_user["leinen"],
+             Ssh_user["casutt"] )
+}
