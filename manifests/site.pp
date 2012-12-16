a8e2583647bd74acc702030ce0dacd8ca9933a71
Simon Leinen <simon.leinen@gmail.com>
2012-12-16 13:42:40 +0100

    Added comment, and tried whether I can put the NTP part after the node declaration.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,14 +1,20 @@
-class { ntp:
-  ensure     => running,
-  servers    => [ "ineri.switch.ch iburst",
-                  "myoko.switch.ch iburst" ],
-  autoupdate => true,
-}
-
 node default {
   include first_file
   include sshd
   include user::virtual
   include user::sysadmins
   include ntp
+}
+
+## We want NTP running on the servers for better synchronization.
+##
+## ineri and myoko are used as NTP servers.  We cannot use arbitrary
+## NTP servers on the Internet (such as the popular "pools") because
+## of filter policy.
+##
+class { ntp:
+  ensure     => running,
+  servers    => [ "ineri.switch.ch iburst",
+                  "myoko.switch.ch iburst" ],
+  autoupdate => true,
 }
