0a8a346cd7d5c7b2ae8bdd7e029e3a891733879f
Simon Leinen <simon.leinen@gmail.com>
2012-12-16 13:30:02 +0100

    Added NTP configuration.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,13 @@
 node default {
-    include first_file
-    include sshd
-    include user::virtual
-    include user::sysadmins
+  include first_file
+  include sshd
+  include user::virtual
+  include user::sysadmins
+  include ntp
+  class { ntp:
+    ensure     => running,
+    servers    => [ "ineri.switch.ch iburst",
+                    "myoko.switch.ch iburst" ],
+    autoupdate => true,
+  }
 }
