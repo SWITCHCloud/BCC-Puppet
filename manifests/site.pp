9f58a2e0b1c681f348f5f01715bd5376eef8b647
Simon Leinen <simon.leinen@gmail.com>
2012-12-16 13:37:44 +0100

    Next attempt at setting parameters for the NTP class.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,14 @@
+class { ntp:
+  ensure     => running,
+  servers    => [ "ineri.switch.ch iburst",
+                  "myoko.switch.ch iburst" ],
+  autoupdate => true,
+}
+
 node default {
   include first_file
   include sshd
   include user::virtual
   include user::sysadmins
   include ntp
-  class { ntp:
-    ensure     => running,
-    servers    => [ "ineri.switch.ch iburst",
-                    "myoko.switch.ch iburst" ],
-    autoupdate => true,
-  }
 }
