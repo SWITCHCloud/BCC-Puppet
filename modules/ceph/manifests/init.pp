d59db698fa8629d3abd3fb5b02495aa1292c74f8
Simon Leinen <simon.leinen@gmail.com>
2012-12-17 11:47:54 +0100

    Fixed stupid comma error.

--- modules/ceph/manifests/init.pp
+++ modules/ceph/manifests/init.pp
@@ -5,12 +5,13 @@ class ceph {
     group => 'root',
     mode => '0644',
     source => 'puppet:///ceph/ceph.conf',
-    },
-    service { 'ceph':
-      ensure => running,
-      hasstatus  => true,
-      hasrestart => true,
-      subscribe => File['ceph'],
-      }
+    }
+
+  service { 'ceph':
+    ensure => running,
+    hasstatus  => true,
+    hasrestart => true,
+    subscribe => File['ceph'],
   }
+}
   
